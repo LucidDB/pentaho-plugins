0b277a8650d9df62e697895f674e1aa55ce1a3db
suresh <suresh@dynamobi.com>
2010-11-13 18:44:14 +0000

    Modified getServerPropertyInfo to include all paramters

--- src/com/dynamobi/sqlmed/pdi/PDIMedForeignDataWrapper.java
+++ src/com/dynamobi/sqlmed/pdi/PDIMedForeignDataWrapper.java
@@ -19,34 +19,38 @@ public class PDIMedForeignDataWrapper extends MedAbstractDataWrapper {
     private static final Logger logger = FarragoTrace.getClassTracer(PDIMedDataServer.class);
     
 	public PDIMedForeignDataWrapper() {
-        logger.log(Level.SEVERE, "################ in method PDIMedForeignDataWrapper()");
-		
+        
 	}
 	
 	public String getDescription(Locale locale) {
-        logger.log(Level.SEVERE, "################ in method PDIMedForeignDataWrapper.getDescription()");
         return "Foreign data wrapper for PDI data";
 	}
 
 
 	public String getSuggestedName() {
-        logger.log(Level.SEVERE, "################ in method PDIMedForeignDataWrapper.getSuggestedName()");
         return "PDI_DATA_WRAPPER";
+	}
+	
+	public DriverPropertyInfo[] getServerPropertyInfo(Locale locale,
+			Properties wrapperProps, Properties serverProps) {
+		
+		MedPropertyInfoMap infoMap = new MedPropertyInfoMap(FarragoResource.instance(), "PDIMed", serverProps);
+		infoMap.addPropInfo(PDIKtrFileParams.PROP_DIRECTORY, true);
+		infoMap.addPropInfo(PDIKtrFileParams.PROP_KTR_FILE,true);
+		infoMap.addPropInfo(PDIKtrFileParams.PROP_FILE_EXTENSION,true,new String[]{PDIKtrFileParams.DEFAULT_FILE_EXTENSION});
+		
+		return infoMap.toArray();
 	}
 
 	public void initialize(
 	        FarragoRepos repos,
 	        Properties props)throws SQLException {
-        logger.log(Level.SEVERE, "################ in method PDIMedForeignDataWrapper.initialize()");
 		super.initialize(repos, props);
 	}
 	
 	public PDIMedDataServer newServer(String serverMofId, Properties props) {
-        logger.log(Level.SEVERE, "################ in method PDIMedForeignDataWrapper.newServer()");
-
-        Properties chainedProps = new Properties(getProperties());
-        chainedProps.putAll(props);
-        PDIMedDataServer server = new PDIMedDataServer(this,serverMofId, chainedProps);
+             
+        PDIMedDataServer server = new PDIMedDataServer(this,serverMofId, props);
         boolean success = false;
         try {
             server.initialize();
@@ -56,22 +60,10 @@ public class PDIMedForeignDataWrapper extends MedAbstractDataWrapper {
 
         } finally {
             if (!success) {
-                //server.closeAllocation();
+                server.closeAllocation();
             }
         }
         return null;
-	}
-	
-	public DriverPropertyInfo[] getServerPropertyInfo(Locale locale,
-			Properties wrapperProps, Properties serverProps) {
-		
-        logger.log(Level.SEVERE, "################ in method PDIMedForeignDataWrapper.getServerPropertyInfo()");
-		MedPropertyInfoMap infoMap = new MedPropertyInfoMap(FarragoResource.instance(), "PDIMed", serverProps);
-		infoMap.addPropInfo(PDIMedDataServer.PROP_PDI_URL, true);
-		infoMap.addPropInfo(PDIMedDataServer.PROP_STEP_TYPE, true, new String[] { PDIMedDataServer.DEFAULT_STYPE_TYPE } );
-		
-		return infoMap.toArray();
-	}
-	
+	}	
 	
 }
